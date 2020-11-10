22947b86f2af1b78e96e027d41493df03daefec3
DESKTOP-TRCC682\fran <zdb17173@gmail.com>
2020-11-11 00:43:45 +0800

    no message

--- reacttest/TUTORIAL.md
+++ reacttest/TUTORIAL.md
@@ -9,3 +9,115 @@ npm start
 使用该命令可快速搭建脚手架，直接开始编程。启动后访问`http://localhost:3000`即可开始预览结果
 
 
+# JSX [link](https://react.docschina.org/docs/introducing-jsx.html)
+
+
+```
+const element = <h1>Hello, world!</h1>;
+```
+这个有趣的标签语法既不是字符串也不是 HTML。它被称为 JSX，是一个 JavaScript 的语法扩展。我们建议在 React 中配合使用 JSX，JSX 可以很好地描述 UI 应该呈现出它应有交互的本质形式。JSX 可能会使人联想到模版语言，但它具有 JavaScript 的全部功能。
+
+JSX必须是一个闭合区间
+```
+//可以
+const element = <div tabIndex="0"></div>;
+
+//可以
+const element = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+);
+
+//报错
+const element = (
+    <div></div>
+    <div></div>
+);
+```
+
+JSX的属性，被{}包起来的内容将被转化为属性，函数将被取值后得到js对象。
+```
+
+const element = <div tabIndex="0"></div>;
+
+const element = <img src={user.avatarUrl}></img>;
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+```
+
+# 元素渲染 [link](https://react.docschina.org/docs/rendering-elements.html)
+
+React 元素是不可变对象。一旦被创建，你就无法更改它的子元素或者属性。一个元素就像电影的单帧：它代表了某个特定时刻的 UI。
+```
+//一个最小的元素快
+const element = <h1>Hello, world</h1>;
+```
+
+根据我们已有的知识，更新 UI 唯一的方式是创建一个全新的元素，并将其传入 ReactDOM.render()。
+
+下面是一个定时器更新的例子，React 只更新它需要更新的部分
+```
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+```
+
+# 组件
+
+组件允许你将 UI 拆分为独立可复用的代码片段，并对每个片段进行独立构思。
+```
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+```
+
+# 拷贝
+
+react的state在修改时尽量采用拷贝方式，以触发react的自动渲染机制。并且后续可方便进行历史回溯等功能的添加。
+```
+const target = { a: 1 };
+
+const source1 = { b: 2 };
+const source2 = { c: 3 };
+
+Object.assign(target, source1, source2);
+target // {a:1, b:2, c:3}
+```
+
+注意
+
+（1）浅拷贝
+Object.assign方法实行的是浅拷贝，而不是深拷贝。也就是说，如果源对象某个属性的值是对象，那么目标对象拷贝得到的是这个对象的引用。
+```
+const obj1 = {a: {b: 1}};
+const obj2 = Object.assign({}, obj1);
+
+obj1.a.b = 2;
+obj2.a.b // 2
+```
+
+（2）同名属性的替换
+
+对于这种嵌套的对象，一旦遇到同名属性，Object.assign的处理方法是替换，而不是添加。
+```
+const target = { a: { b: 'c', d: 'e' } }
+const source = { a: { b: 'hello' } }
+Object.assign(target, source)
+// { a: { b: 'hello' } }
+```
+
+
+
+
--- reacttest/src/game.js
+++ reacttest/src/game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-class Square extends React.Component {
+  //普通react组件写法
+  class Square extends React.Component {
 
     render() {
       return (
@@ -9,6 +10,35 @@ class Square extends React.Component {
         </button>
       );
     }
+  }
+
+  //函数式组件写法（不依赖state） 使用方式和上面的标准写法一样
+  function SquareFun(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   class Board extends React.Component {
@@ -21,11 +51,16 @@ class Square extends React.Component {
     }
 
     handleClick(i) {
+        //不直接修改（或改变底层数据）
+        //可实现一种叫做“时间旅行”的功能。“时间旅行”可以使我们回顾井字棋的历史步骤
+        //不可变性最主要的优势在于它可以帮助我们在 React 中创建 pure components。我们可以很轻松的确定不可变数据是否发生了改变，从而确定何时对组件进行重新渲染。
         const squares = this.state.squares.slice();
-        if(squares[i]){
-            alert("checked")
+        const winner = calculateWinner(this.state.squares);
+        if(winner || squares[i]){
+            //alert("checked")
             return;
         }
+
         squares[i] = this.state.cur;
         if(this.state.cur === 'X')
             this.setState({cur :'O'});
@@ -36,12 +71,18 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+      return <SquareFun value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
     }
   
     render() {
-      const status = 'Next player: ' + this.state.cur;
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + this.state.cur;
+      }
+      
       return (
         <div>
           <div className="status">{status}</div>
